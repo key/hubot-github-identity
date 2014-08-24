5d8153aa883251ca15e4e5b3bf3c57c3488a2615
Tom Bell <tomb@tomb.io>
2014-08-24 18:55:06 +0100

    Add the initial views and handling of replies/commands

--- templates/javascripts/application.js
+++ templates/javascripts/application.js
@@ -1,4 +1,62 @@
 window.App = {
   init: function() {
+    this.appView = new window.AppView;
   },
 };
+
+window.AppView = Backbone.View.extend({
+  initialize: function() {
+    this.commandbar = new window.CommandBarView;
+  },
+});
+
+window.RepliesView = Backbone.View.extend({
+  el: '.js-replies',
+
+  add: function(reply) {
+    this.$el.append(reply.render().el);
+  },
+
+  fadePrevious: function() {
+    this.$('h1').css('opacity', '.2');
+  },
+});
+
+window.ReplyView = Backbone.View.extend({
+  tagName: 'h1',
+
+  initialize: function(options) {
+    this.template = _.template($(options.template).html());
+  },
+
+  render: function() {
+    this.$el.html(this.template());
+    console.log(this.$el);
+    return this;
+  },
+});
+
+window.CommandBarView = Backbone.View.extend({
+  el: '.js-command-bar',
+
+  events: {
+    'keypress .js-command-input': 'submit',
+  },
+
+  initialize: function() {
+    this.commandinput = this.$('.js-command-input');
+    this.commandinput.focus();
+
+    this.replies = new window.RepliesView;
+  },
+
+  submit: function(e) {
+    if (e.keyCode != 13) return;
+    if (!this.commandinput.val()) return;
+
+    var reply = new window.ReplyView({ template: '#user-success' });
+    this.replies.fadePrevious();
+    this.replies.add(reply);
+    this.commandinput.val('');
+  },
+});
